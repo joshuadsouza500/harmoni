9899caf6467cac79069c8f59f012777c874f780f
joshua <joshuadsouza500@gmail.com>
2025-02-19 00:33:43 +0300

    md and lg sizes configured and prd hotspot

---
+++ app/components/ProductHotspot.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+{
+  /**To show product hover card when hovered in shop by loks section */
+}
+const ProductHotspot = () => {
+  return <div>ProductHotspot</div>;
+};
+
+export default ProductHotspot;
