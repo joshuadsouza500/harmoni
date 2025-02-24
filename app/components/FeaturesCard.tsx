b33f2cbc33859c95b8be330ec03d45c3be59d6ca
joshua <joshuadsouza500@gmail.com>
2025-02-24 23:53:05 +0300

    Added colours check page.tsx

--- app/components/FeaturesCard.tsx
+++ app/components/FeaturesCard.tsx
@@ -14,7 +14,7 @@ const FeaturesCard = ({
   description,
 }: FeaturesCardProps) => {
   return (
-    <div className=" relative py-3 md:py-6 px-1 md:px-4 2xl:py-8  rounded-2xl bg-white  transition-all duration-300 shadow-md hover:shadow-lg hover:-translate-y-1 group hover:border-[0.5px] border-[0.2px]">
+    <div className=" relative py-3 md:py-6 px-1 md:px-4 2xl:py-8  rounded-2xl bg-white  transition-all duration-300 shadow-md hover:shadow-lg hover:-translate-y-1 group hover:border-[0.5px] border-[0.2px] shadow-[#C9B3A2]/20">
       <div
         className={`w-12 h-12 rounded-full bg-[#F6EDE7] flex items-center justify-center mb-4 mx-auto   `}
       >
@@ -23,7 +23,7 @@ const FeaturesCard = ({
       <h3 className="text-lg lg:text-xl font-semibold text-center mb-2 text-gray-800">
         {title}
       </h3>
-      <p className="text-sm text-center text-[#C9B3A2] gray-600 tracking-wide text-balance lg:leading-relaxed line-clamp-3">
+      <p className="text-sm text-center text-[#77573f] gray-600 tracking-wide text-balance lg:leading-relaxed line-clamp-3">
         {description}
       </p>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ icon bgL:[#F6EDE7]
 }
 export default function Home() {
   return (
-    <div className=" bg- white font-helvetica py-2 [#F3F0EA]  bg-[#f7f7f7f8]">
+    <div className="  font-sans py-2 [#F3F0EA]  bg-white [#f7f7f7f8]">
       <Hero />
       <About />
       <NewCollection />
