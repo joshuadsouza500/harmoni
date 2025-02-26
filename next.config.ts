82a854592012d2ee1c02cb3647208d84aa29f582
joshua <joshuadsouza500@gmail.com>
2025-02-26 23:06:06 +0300

    images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here add dofferent sites from where images are taken */
 
   images: {
     remotePatterns: [
